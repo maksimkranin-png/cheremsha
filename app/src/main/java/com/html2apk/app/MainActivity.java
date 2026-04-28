fd85876e15a9f786da32ef0dfbe04a902205ff35
WaxedGames <maksim.kranin@mail.ru>
2026-04-28 16:08:00 +0300

    Add files via upload

---
+++ app/src/main/java/com/html2apk/app/MainActivity.java
@@ -0,0 +1,18 @@
+package com.html2apk.app;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+        setContentView(webView);
+    }
+}
